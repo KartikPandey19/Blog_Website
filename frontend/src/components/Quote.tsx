6eed589d53f9679ca1133150779da323e1993910
KartikPandey19 <kartik2000pandey@gmail.com>
2024-08-01 09:23:29 +0530

    init

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,20 @@
+export const Quote = () =>{
+
+    return <div className="bg-slate-100 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+        <div className="max-w-lg">
+            <div className="font-bold text-3xl">
+        “Life is fragile. We’re not guaranteed a tomorrow, so give it everything you’ve got.” 
+        </div>
+        
+        <div className="max-w-md font-semibold text-xl mt-4">
+           Tim Cook
+        </div>
+        <div className="max-w-md text-gray-400 font-light text-sm">
+           CEO of Apple
+        </div>
+        </div>
+        </div>
+        
+    </div>
+}

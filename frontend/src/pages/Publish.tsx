df877f8fe832d45c4f91f382904cdfbb87300b6d
KartikPandey19 <kartik2000pandey@gmail.com>
2025-01-29 00:19:24 +0530

    Save fix

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -12,7 +12,6 @@ import "react-quill/dist/quill.snow.css";
 export const Publish = () => {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
-    const quillRef = useRef();
     const navigate = useNavigate();
     const saveBlog = async()=> {
     const response = await axios.post(`${BACKEND_URL}/api/v1/blog`,{
@@ -25,10 +24,7 @@ export const Publish = () => {
         });
         navigate(`/blog/${response.data.id}`)
        } 
-       const handleSave = (content: string) => {
-        setDescription(content);
-        saveBlog();
-  };
+       
   const myColors = [
     "purple",
     "#785412",
@@ -90,17 +86,7 @@ export const Publish = () => {
                     value={description}
                     onChange={handleProcedureContentChange}
                   />
-            <button onClick={async()=>{
-            const response = await axios.post(`${BACKEND_URL}/api/v1/blog`,{
-            title,
-            content:description
-        },{
-            headers:{
-                Authorization: localStorage.getItem("token")
-            }
-        });
-        navigate(`/blog/${response.data.id}`)
-       }} type="submit" className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 mt-4 hover:bg-blue-800">
+            <button onClick={saveBlog} className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 mt-4 hover:bg-blue-800">
            Publish post
        </button>
             </div>
@@ -113,16 +99,3 @@ export const Publish = () => {
     )
 }
 
-function TextEditor({onChange}:{onChange:(e:ChangeEvent<HTMLTextAreaElement>)=>void}){
-    return <div className="mt-2">
-       <div className="w-full mb-4">
-          <div className="flex items-center justify-between border">
-           <div className="my-2 bg-white rounded-b-lg w-full">
-               <textarea onChange={onChange} rows={8} className="focus:outline-none block w-full px-0 text-sm text-gray-800 bg-white border-0 pl-2" placeholder="Content for article..." required ></textarea>
-           </div>
-       </div>
-       
-       </div>
-    </div>
-    
-}
